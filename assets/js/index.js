8fdb78c4b470dc0c70ee109505581ddd5e41c096
c-navarroperez <carlosnavarro.brs@gmail.com>
2022-11-15 22:35:37 +0000

    Adding calculations and console prints for the greatest increase in profits and the greatest decrease in profits.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 /*
-You have been given a dataset composed of arrays with two fields, Date and Profit/Losses.
-Your task is to write JavaScript code that analyzes the records to calculate each of the following:
+The JavaScript file finances.js contains a dataset composed of arrays with two fields, Date and Profit/Losses.
+The following JavaScript code analyzes the records.
 */
 
 /* Global Variables */ 
@@ -8,43 +8,56 @@ var totNumMonth = 0;
 var totProfitLoss = 0;
 var monthChangeTot = 0;
 var avgMonthChange = 0
+var greatestIncrease = 0;
+var greatestIncreaseDate = 0;
+var greatestDecrease = 0;
+var greatestDecreaseDate = 0;
 
 
-/* Main body */
+/* Main Code */
 
 // Total number of months included in the dataset.
 totNumMonth = finances.length;
 
-// Net total amount of Profit/Losses over the entire period.
 for (var i = 0; i < finances.length; i++ ) {
     
     var monthChange = 0;    
 
     for(var j = 1; j < finances[i].length; j++) {
+        // Net total amount of Profit/Losses over the entire period.
         totProfitLoss += finances[i][j];
 
-        // Average of the changes in Profit/Losses over the entire period.
+        // Month to month comparisons will number one less than the total number of months
         if (i >= finances.length - 1) {
             break;
         } 
         // Total change in profits from month to month
         monthChange = finances[i+1][j] - finances[i][j];
         monthChangeTot += monthChange;
+
+        // Greatest increase in profits (date and amount) over the entire period.
+        if (greatestIncrease <= monthChange) {
+            greatestIncrease = monthChange;
+            greatestIncreaseDate = finances[i+1][0];
+        } 
+        // Greatest decrease in losses (date and amount) over the entire period.
+        else if (greatestDecrease >= monthChange){
+            greatestDecrease = monthChange;
+            greatestDecreaseDate = finances[i+1][0];
+        }
     } 
 }
-// Month to month average change in profits.
+// Average month to month change in profits over the entire period.
 avgMonthChange = monthChangeTot/(totNumMonth - 1);
-
 
 /* Log to console */ 
 console.log("Financial Analysis \n----------------------------");
 console.log("Total months: ", totNumMonth);
 console.log("Total: $" + totProfitLoss);
 console.log("Average Change: " + avgMonthChange.toFixed(2));
+console.log(`Greatest Increase in Profits: ${greatestIncreaseDate} ($${greatestIncrease})`);
+console.log(`Greatest Dcrease in Profits: ${greatestDecreaseDate} ($${greatestDecrease})`);
+
       
 
 
-    // The greatest increase in profits (date and amount) over the entire period.
-
-
-    // The greatest decrease in losses (date and amount) over the entire period.
